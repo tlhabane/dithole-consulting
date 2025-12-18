4588416c8b6da0d3a22751c7c51ce162da0a3764
Tlhabane Monareng <tmonareng@kreativware.co.za>
2025-12-18 14:24:56 +0200

    Updated php files

--- enquiry.php
+++ enquiry.php
@@ -22,7 +22,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
         $headers 	.= "MIME-Version: 1.0\r\n";
         $headers 	.= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-        $header		= '<html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head><body style="margin: 0; padding: 15px; background-color: #ffffff; font-family: Tahoma, Geneva, sans-serif; font-size: 14px; color: #525252; line-height: 17px;"><p><a href="'. SITE_ROOT .'"><img src="' . SITE_LOGO . '" /></a></p>';
+        $header		= '<html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head><body style="margin: 0; padding: 15px; background-color: #ffffff; font-family: Tahoma, Geneva, sans-serif; font-size: 14px; color: #525252; line-height: 17px;"><p><a href="'. SITE_ROOT .'"><img src="' . SITE_LOGO . '" width="200" /></a></p>';
 
         $footer		= '<p style="text-align: left; color: #525252; font-size: 10px; line-height: 11px;"><strong>Attention:</strong><br />The information contained in this message and or attachments is intended only for the person or entity to which it is addressed and may contain confidential and/or privileged material.  Any review, retransmission, dissemination or other use of, or taking of any action in reliance upon, this information by persons or entities other than the intended recipient is prohibited. If you received this in error, please contact the sender and delete the material from any system and destroy any copies.<br /><br />Thank You.</p></body></html>';
 
